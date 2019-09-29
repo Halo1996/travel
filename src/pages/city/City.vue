c7bef593c40984b9e3d96a07fba60534de973933
zhanghao_2019 <zxcvbnm123>
2019-09-29 14:20:40 +0800

    change

---
+++ src/pages/city/City.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <!--<home-swiper :swiperList="swiperList"></home-swiper>
+    <home-icons :iconList="iconList"></home-icons>
+    <home-recommend :recommendList="recommendList"></home-recommend>
+    <home-weekend :weekendList="weekendList"></home-weekend>-->
+  </div>
+</template>
+<script>
+import CityHeader from './components/Header'
+// import HomeSwiper from './components/Swiper'
+// import HomeIcons from './components/Icons'
+// import HomeRecommend from './components/Recommend'
+// import HomeWeekend from './components/Weekend'
+import axios from 'axios'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+    // HomeSwiper,
+    // HomeIcons,
+    // HomeRecommend,
+    // HomeWeekend
+  },
+  data() {
+    return {
+      city: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  methods: {
+    getHomeInfo() {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc(res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+        console.log(this.swiperList)
+      }
+    }
+  },
+  mounted() {
+    this.getHomeInfo()
+  }
+}
+</script>
+<style lang="stylus" scoped></style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,10 +6,12 @@
     <div class="header-input">
       <span class="iconfont">&#xe605;</span> 输入城市/景点/游玩/主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe607;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe607;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 <script>
@@ -56,6 +58,7 @@ export default {
     width: 1.24rem;
     float: right;
     text-align: center;
+    color: white;
 
     .arrow-icon {
       margin-left: -0.04rem;
